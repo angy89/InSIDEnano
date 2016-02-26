4b106d1c69e1967ffbce3cb668ce720117eb494e
angy89 <aserra@unisa.it>
2016-02-26 15:01:19 +0100

    single item query GUI

---
+++ GUI/items_analisys_UI.R
@@ -0,0 +1,145 @@
+items_query_UI = function(input,output){
+  output$disease_couple = renderUI({
+    selectizeInput(
+      'disease_couple', label = "Select the item to start the analysis", choices = c(disease,nano,drugs,chemical), multiple = FALSE,
+      options = list(create = TRUE)
+    )
+  })
+  
+  output$th_slider_couple = renderUI({
+    sliderInput("th_slider_couple", label = "Strenght of Similarity",
+                min = 0, max = 99, value = 99,step=1)
+  })  
+  
+  output$Go_couple = renderUI({
+    actionButton(inputId = "Go_couple",label  = "Start",icon("circle-arrow-right", lib = "glyphicon"))
+  })
+  
+  output$refresh_couple = renderUI({
+    actionButton(inputId = "refresh_couple", label  = "Refresh",icon("refresh", lib = "glyphicon"))
+  })
+}
+
+free_query_UI_refresh = function(input,output){
+  output$disease = renderUI({
+    selectizeInput(
+      'disease', label = "Select the item to start the analysis", choices = c(disease,nano,drugs,chemical), multiple = TRUE,
+      options = list(create = TRUE)
+    )
+  })
+  
+  output$th_slider = renderUI({
+    sliderInput("th_slider", label = "Strenght of Similarity",
+                min = 0, max = 99, value = 99,step=1)
+  })
+  
+  output$NodesOfInterest_totale <- renderUI({
+    selectInput("InterestingNodes_totale",label = "Node of Interest",multiple = TRUE,choices = disease_list,selected = disease_list[[1]])
+  })
+  
+  output$plotTripel_total = renderUI({
+    selectInput("plotTripel_total",label = "Plot of Association Frequencies",
+                choices = list("Disease-Nano-Drug" = 1,
+                               "Disease-Nano-Chemical"= 2,
+                               "Disease-Nano"=3,
+                               "Disease-Drug"=4,
+                               "Disease-Chemical"=5,
+                               "Chemical-Nano" = 6,
+                               "Chemical-Drug" = 7,
+                               "Nano-Drug" = 8),selected = 1)
+  })
+  
+}
+
+# free_query_UI_node_of_interest_output = function(input,output,disease_list){
+#   
+#   output$NodesOfInterest <- renderUI({
+#     selectInput("NodesOfInterest",label = "Node of Interest",multiple = TRUE,choices = disease_list,selected = disease_list[[1]])
+#   })
+#   
+#   output$NodesOfInterest_totale <- renderUI({
+#     selectInput("InterestingNodes_totale",label = "Node of Interest",multiple = TRUE,choices = disease_list,selected = disease_list[[1]])
+#   })
+#   
+#   output$NodesOfInterest_items <- renderUI({
+#     selectInput("InterestingNodes_items",label = "Node of Interest",multiple = FALSE,choices = disease_list,selected = disease_list)
+#   })
+#   
+# }
+# 
+# free_query_UI_ggplot_totale = function(input,output,MList,graph_gw){
+#   output$ggplot_totale = renderPlot({
+#     triple_type = input$plotTripel_total
+#     
+#     Mi = MList[[1]]
+#     for(index in 2:length(MList)){
+#       Mi = rbind(Mi,MList[[index]])
+#     }
+#     
+#     Mi = as.data.frame(Mi)
+#     colnames(Mi)=c("Disease","Nano","Drug","Chemical")
+#     
+#     if(triple_type==1){
+#       columns_ = c("Disease","Nano","Drug")
+#     }
+#     if(triple_type==2){
+#       columns_ = c("Disease","Nano","Chemical")
+#     }
+#     if(triple_type==3){
+#       columns_ = c("Disease","Nano")
+#     }
+#     if(triple_type==4){
+#       columns_ = c("Disease","Drug")
+#     }
+#     if(triple_type==5){
+#       columns_ = c("Disease","Chemical")
+#     }
+#     
+#     Mi_count = count(Mi, vars=columns_)
+#     nano_id = unique(Mi_count$Nano)
+#     drug_id = unique(Mi_count$Drug)
+#     disease_id = unique(Mi_count$Disease)
+#     
+#     Mi_count = Mi_count[order(Mi_count$freq),]
+#     nElem_c = length(columns_)
+#     
+#     d_id = input$InterestingNodes_totale
+#     d_node_type = igraph::get.vertex.attribute(graph = graph_gw,name = "type",index = d_id)
+#     column_index = which(c("disease","nano","drugs","chemical") %in% d_node_type)
+#     
+#     indexes_c = 1:(dim(Mi_count)[2]-1)
+#     indexes_c = indexes_c[-column_index]
+#     
+#     if(length(columns_)>2){
+#       nElem = round((dim(Mi_count)[1]) * 0.10,digits = 0)
+#       Mi_count2 = Mi_count$freq[1:nElem]
+#       names(Mi_count2) =  paste(Mi_count[1:nElem,indexes_c[1]],Mi_count[1:nElem,indexes_c[2]],sep="-")
+#       
+#     }
+#     if(length(columns_) == 2){
+#       nElem = dim(Mi_count)[1]
+#       Mi_count2 = Mi_count$freq[1:nElem]
+#       names(Mi_count2) =  paste(Mi_count[1:nElem,columns_[2:nElem_c]],sep=" ")#,Mi_count[1:nElem,3],sep=" ")
+#       
+#     }
+#     
+#     index = which(Mi_count[1:nElem,column_index] %in% d_id) 
+#     
+#     validate(
+#       need(length(index)>0, paste("No clique for", d_id ,"with this threshold"))
+#     )
+#     
+#     mar.default = c(5,4,4,2) + 0.1
+#     par(mar = mar.default+ c(0,15,0,0))
+#     
+#     validate(
+#       need(length(Mi_count2[index])>0, "No correspondence for the selected criteria!")
+#     )
+#     
+#     barplot(as.vector(Mi_count2[index]),horiz = TRUE,col = rainbow(length(index)),main=d_id,
+#             names.arg=names(Mi_count2)[index], cex.names=1, las=1)
+#     
+#   })
+# }
+
+
---
+++ query/couple_query.R
@@ -0,0 +1,331 @@
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+couple_query = function(input,output,disease_list,selected_nodes,W_ADJ,th_p = input$th_slider/100,node_type,chemMat,join10,g,g_geni2,items_list){
+  output$infoFree <- renderUI({HTML(info_free_query_text)}) 
+  
+  withProgress(message = 'Progress...', min = 1,max = 10, {
+    validate(need(input$disease_couple != "", "Please select a Disease"))
+    
+    query_node = input$disease_couple
+    if(DEBUGGING){
+      message("query_node:: ",query_node,"\n")
+      message("input$th_slider/100:: ",th_p,"\n")
+    }
+    
+    incProgress(1, detail = "Thresholding...")
+  
+    THS = find_thresholds(W_ADJ,th_p) #in query_utilities.R
+    
+    incProgress(1, detail = "Removing edges under threshold...")
+    W_ADJ = apply_thresholds(W_ADJ,THS) #in query_utilities.R
+    
+    incProgress(1, detail = "Creating graph...")
+    graph_gw = creating_graph(W_ADJ,node_type) #in query_utilities.R
+       
+    incProgress(1, detail = "Creating results...")
+    
+    tab <- data.frame(cbind(
+      name=V(graph_gw)[neighbors(graph_gw,query_node, mode='out')]$name,
+      weight=E(graph_gw)[query_node %->% neighbors(graph_gw,query_node, mode='out')]$weight))
+
+    
+    tab[,2] = round(as.numeric(as.vector(tab$weight)),2)
+    tab <- tab[order(tab$weight, decreasing=TRUE),]
+    
+    nano_t = tab[tab$name %in% nano,]
+    drug_t = tab[tab$name %in% drugs,]
+    chemical_t = tab[tab$name %in% chemical,]
+    disease_t = tab[tab$name %in% disease,]
+    
+    
+    output$boxplot_statistics = renderPlot({
+      num_nano = as.numeric(nano_t[,2])
+      num_drug = as.numeric(drug_t[,2])
+      num_chem = as.numeric(chemical_t[,2])
+      num_dise = as.numeric(disease_t[,2])
+      
+      nn_p = num_nano[num_nano>=0]
+      nn_n = num_nano[num_nano<0]
+      
+      ndr_p = num_drug[num_drug>=0]
+      ndr_n = num_drug[num_drug<0]
+      
+      nc_p = num_chem[num_chem>=0]
+      nc_n = num_chem[num_chem<0]
+      
+      ndi_p = num_dise[num_dise>=0]
+      ndi_n = num_dise[num_dise<0]
+      
+      par(mfrow=c(2,1),oma = c(5,4,0,0) + 0.1,mar=c(1, 1, 1, 12) + 0.1,xpd=TRUE)
+      boxplot(nn_p,ndr_p,nc_p,ndi_p,col="red",main="Connections Weight",names=c("Nano","Drugs","Chemical","Disease"),horizontal = TRUE)
+      legend(x = "topright",inset=c(-0.2,0),legend = c("Positive","Negative"),fill=c("red","green")) 
+      boxplot(nn_n,ndr_n,nc_n,ndi_n,col="green",names=c("Nano","Drugs","Chemical","Disease"),horizontal = TRUE)
+      
+#       legend(x = "topright",inset=c(-0.2,0),legend = c("Positive","Negative"),fill=c("red","green")) 
+      
+#       nn_p = range01(num_nano[num_nano>=0])
+#       nn_n = range01(abs(num_nano[num_nano<0]))*-1
+#       
+#       ndr_p = range01(num_drug[num_drug>=0])
+#       ndr_n = range01(abs(num_drug[num_drug<0])) * -1
+#       
+#       nc_p = range01(num_chem[num_chem>=0])
+#       nc_n = range01(abs(num_chem[num_chem<0])) * -1
+#       
+#       ndi_p = range01(num_dise[num_dise>=0])
+#       ndi_n = range01(abs(num_dise[num_dise<0])) * -1
+          
+#       boxplot(nn_p,nn_n,ndr_p,ndr_n,nc_p,nc_n,ndi_p,ndi_n,col=rep(c("red","green"),4),
+#               names=c(rep("Nano",2),rep("Drugs",2),rep("Chemicals",2),rep("Diseases",2)))
+#       legend(x = "topright",legend = c("Positive Connections","Negative Connections"),fill=c("red","green"))      
+    })
+    
+    output$barplot_statistics = renderPlot({
+      num_nano = as.numeric(nano_t[,2])
+      num_drug = as.numeric(drug_t[,2])
+      num_chem = as.numeric(chemical_t[,2])
+      num_dise = as.numeric(disease_t[,2])
+      
+      nn_p = num_nano[num_nano>=0]
+      nn_n = num_nano[num_nano<0]
+      
+      ndr_p = num_drug[num_drug>=0]
+      ndr_n = num_drug[num_drug<0]
+      
+      nc_p = num_chem[num_chem>=0]
+      nc_n = num_chem[num_chem<0]
+      
+      ndi_p = num_dise[num_dise>=0]
+      ndi_n = num_dise[num_dise<0]
+      
+      vect_p = c(length(nn_p),length(ndr_p),length(nc_p),length(ndi_p))
+      vect_n = c(length(nn_n),length(ndr_n),length(nc_n),length(ndi_n))
+      par(mfrow=c(2,1),xpd=TRUE,oma = c(5,4,0,0) + 0.1,mar=c(1, 2, 2, 1) + 0.1)
+      barplot(vect_p,col="red",main = "Number of positive connections")      
+      barplot(vect_n,col="green",main="Number of negative connections")
+      #       legend(x = "topright",inset=c(-0.2,0),legend = c("Positive","Negative"),fill=c("red","green")) 
+      
+      #       nn_p = range01(num_nano[num_nano>=0])
+      #       nn_n = range01(abs(num_nano[num_nano<0]))*-1
+      #       
+      #       ndr_p = range01(num_drug[num_drug>=0])
+      #       ndr_n = range01(abs(num_drug[num_drug<0])) * -1
+      #       
+      #       nc_p = range01(num_chem[num_chem>=0])
+      #       nc_n = range01(abs(num_chem[num_chem<0])) * -1
+      #       
+      #       ndi_p = range01(num_dise[num_dise>=0])
+      #       ndi_n = range01(abs(num_dise[num_dise<0])) * -1
+      
+      #       boxplot(nn_p,nn_n,ndr_p,ndr_n,nc_p,nc_n,ndi_p,ndi_n,col=rep(c("red","green"),4),
+      #               names=c(rep("Nano",2),rep("Drugs",2),rep("Chemicals",2),rep("Diseases",2)))
+      #       legend(x = "topright",legend = c("Positive Connections","Negative Connections"),fill=c("red","green"))      
+    })
+
+    
+    if(nrow(nano_t)>0){
+#       num_nano = as.numeric(nano_t[,2])
+#       
+#       num_nano[num_nano>=0] = num_nano[num_nano>=0]
+#       num_nano[num_nano<0] = num_nano[num_nano<0]
+#       
+#       nano_t[,2] = round(num_nano,2)
+#       nano_t = nano_t[order(nano_t[,2],decreasing = TRUE),]
+#       
+#       datTab = matrix(0,nrow = 1,ncol = 2)
+      
+      nano_t = as.matrix(nano_t)
+      
+      for(i in 1:nrow(nano_t)){
+        if(as.numeric(nano_t[i,2])>=0){
+        #  datTab = rbind(datTab,c(paste('<font color="red"><b>',as.character(nano_t[i,1]),'</b></font>'),nano_t[i,2]))
+           nano_t[i,1] = paste('<font color="red"><b>',as.character(nano_t[i,1]),'</b></font>')
+           nano_t[i,2] = nano_t[i,2]
+        }else{
+           nano_t[i,1] = paste('<font color="green"><b>',as.character(nano_t[i,1]),'</b></font>')
+           nano_t[i,2] = nano_t[i,2]      
+#          datTab = rbind(datTab,c(paste('<font color="green"><b>',as.character(nano_t[i,1]),'</b></font>'),nano_t[i,2]))
+          
+        }
+      }
+      #datTab = datTab[-1,]
+      #colnames(datTab) = c("Name","Weight")
+      output$nano_couple_table = renderDataTable({
+        validate(need(nrow(nano_t)>0,"No connections with nano"))
+        DT::datatable(nano_t,escape=FALSE,selection = "single")
+      })
+      
+      
+    }
+    
+    if(nrow(drug_t)>0){
+      
+        drug_t = as.matrix(drug_t)
+      
+#       num_nano = as.numeric(drug_t[,2])
+#       
+#       num_nano[num_nano>=0] = num_nano[num_nano>=0]
+#       num_nano[num_nano<0] = num_nano[num_nano<0]
+#       
+#       drug_t[,2] = round(num_nano,2)
+#       drug_t = drug_t[order(as.numeric(drug_t[,2]),decreasing = TRUE),]
+# 
+#       #datTab = matrix(0,nrow = 1,ncol = 2)
+      
+      for(i in 1:nrow(drug_t)){
+        if(as.numeric(drug_t[i,2])>=0){
+          #datTab = rbind(datTab,c(paste('<font color="red"><b>',as.character(drug_t[i,1]),'</b></font>'),drug_t[i,2]))
+                     drug_t[i,1] = paste('<font color="red"><b>',as.character(drug_t[i,1]),'</b></font>')
+                     drug_t[i,2] = drug_t[i,2]
+        }else{
+                     drug_t[i,1] = paste('<font color="green"><b>',as.character(drug_t[i,1]),'</b></font>')
+                     drug_t[i,2] = drug_t[i,2]      
+          #datTab = rbind(datTab,c(paste('<font color="green"><b>',as.character(drug_t[i,1]),'</b></font>'),drug_t[i,2]))
+          
+        }
+      }
+      
+#       datTab = datTab[-1,]
+#       colnames(datTab) = c("Name","Weight")
+      output$drug_couple_table = renderDataTable({
+        validate(need(nrow(drug_t)>0,"No connections with drugs"))
+        DT::datatable(drug_t,escape=FALSE,selection = "single") 
+      })
+    }
+    
+    if(nrow(chemical_t)>0){
+      chemical_t = as.matrix(chemical_t)
+#       num_nano = as.numeric(chemical_t[,2])
+#       
+#       num_nano[num_nano>=0] = num_nano[num_nano>=0]
+#       num_nano[num_nano<0] = num_nano[num_nano<0]
+#       
+#       chemical_t[,2] = round(num_nano,2)
+#       chemical_t = chemical_t[order(chemical_t[,2],decreasing = TRUE),]
+#       
+#       datTab = matrix(0,nrow = 1,ncol = 2)
+      
+      for(i in 1:nrow(chemical_t)){
+        if(as.numeric(chemical_t[i,2])>=0){
+          #datTab = rbind(datTab,c(paste('<font color="red"><b>',as.character(chemical_t[i,1]),'</b></font>'),chemical_t[i,2]))
+                    chemical_t[i,1] = paste('<font color="red"><b>',as.character(chemical_t[i,1]),'</b></font>')
+                    chemical_t[i,2] = chemical_t[i,2]
+        }else{
+                    chemical_t[i,1] = paste('<font color="green"><b>',as.character(chemical_t[i,1]),'</b></font>')
+                    chemical_t[i,2] = chemical_t[i,2]      
+          #datTab = rbind(datTab,c(paste('<font color="green"><b>',as.character(chemical_t[i,1]),'</b></font>'),chemical_t[i,2]))
+          
+        }
+      }
+      
+#       datTab = datTab[-1,]
+#       colnames(datTab) = c("Name","Weight")
+      output$chemical_couple_table = renderDataTable({
+        validate(need(nrow(chemical_t)>0,"No connections with drugs"))
+        DT::datatable(chemical_t,escape=FALSE,selection = "single") 
+      })
+    }
+    
+    if(nrow(disease_t)>0){
+      disease_t = as.matrix(disease_t)
+#       num_nano = as.numeric(disease_t[,2])
+#       
+#       num_nano[num_nano>=0] = num_nano[num_nano>=0]
+#       num_nano[num_nano<0] = num_nano[num_nano<0]
+#       
+#       disease_t[,2] = round(num_nano,2)
+#       disease_t = disease_t[order(disease_t[,2],decreasing = TRUE),]
+#       
+#       
+#       datTab = matrix(0,nrow = 1,ncol = 2)
+      
+      for(i in 1:nrow(disease_t)){
+        if(as.numeric(disease_t[i,2])>=0){
+          #datTab = rbind(datTab,c(paste('<font color="red"><b>',as.character(disease_t[i,1]),'</b></font>'),disease_t[i,2]))
+                    disease_t[i,1] = paste('<font color="red"><b>',as.character(disease_t[i,1]),'</b></font>')
+                    disease_t[i,2] = disease_t[i,2]
+        }else{
+                    disease_t[i,1] = paste('<font color="green"><b>',as.character(disease_t[i,1]),'</b></font>')
+                    disease_t[i,2] = disease_t[i,2]      
+          #datTab = rbind(datTab,c(paste('<font color="green"><b>',as.character(disease_t[i,1]),'</b></font>'),disease_t[i,2]))
+          
+        }
+      }
+      
+#       datTab = datTab[-1,]
+#       colnames(datTab) = c("Name","Weight")
+      output$disease_couple_table = renderDataTable({
+        validate(need(nrow(disease_t)>0,"No connections with drugs"))
+        DT::datatable(disease_t,escape=FALSE,selection = "single") 
+      })
+    }
+    
+    #     output$nano_couple_boxplot = renderPlot({
+    #       validate(need(nrow(nano_t)>0,"No connections with nano"))
+    #       num = as.numeric(nano_t[,2])
+    #       idx_p = which(num>=0)
+    #       idx_n = which(num<0)
+    #       if(length(idx_n)==0){
+    #         boxplot(num[idx_p],main="Nanomaterials connections",col = "red",axes=FALSE)
+    #         axis(2)
+    #         legend(x = "topright",legend = "Positive",fill="red")
+    #       }else{
+    #         boxplot(num[idx_p],num[idx_n],main="Nanomaterials connections",axes=FALSE,col = c("red","green"))
+    #         axis(2)
+    #         legend(x = "topright",legend = c("Positive","Negative"),fill=c("red","green"))
+    #       }
+    #     })
+    #     
+    #     output$drug_couple_boxplot = renderPlot({
+    #       validate(need(nrow(drug_t)>0,"No connections with drug"))
+    #       num = as.numeric(drug_t[,2])
+    #       idx_p = which(num>=0)
+    #       idx_n = which(num<0)
+    #       if(length(idx_n)==0){
+    #         boxplot(num[idx_p],main="drugmaterials connections",col = "red",axes=FALSE)
+    #         axis(2)
+    #         legend(x = "topright",legend = "Positive",fill="red")
+    #       }else{
+    #         boxplot(num[idx_p],num[idx_n],main="drugmaterials connections",axes=FALSE,col = c("red","green"))
+    #         axis(2)
+    #         legend(x = "topright",legend = c("Positive","Negative"),fill=c("red","green"))
+    #       }
+    #     })
+    #     
+    #     output$chemical_couple_boxplot = renderPlot({
+    #       validate(need(nrow(chemical_t)>0,"No connections with chemical"))
+    #       num = as.numeric(chemical_t[,2])
+    #       idx_p = which(num>=0)
+    #       idx_n = which(num<0)
+    #       if(length(idx_n)==0){
+    #         boxplot(num[idx_p],main="chemicalmaterials connections",col = "red",axes=FALSE)
+    #         axis(2)
+    #         legend(x = "topright",legend = "Positive",fill="red")
+    #       }else{
+    #         boxplot(num[idx_p],num[idx_n],main="chemicalmaterials connections",axes=FALSE,col = c("red","green"))
+    #         axis(2)
+    #         legend(x = "topright",legend = c("Positive","Negative"),fill=c("red","green"))
+    #       }
+    #     })
+    #     
+    #     output$disease_couple_boxplot = renderPlot({
+    #       validate(need(nrow(disease_t)>0,"No connections with disease"))
+    #       num = as.numeric(disease_t[,2])
+    #       idx_p = which(num>=0)
+    #       idx_n = which(num<0)
+    #       if(length(idx_n)==0){
+    #         boxplot(num[idx_p],main="diseasematerials connections",col = "red",axes=FALSE)
+    #         axis(2)
+    #         legend(x = "topright",legend = "Positive",fill="red")
+    #       }else{
+    #         boxplot(num[idx_p],num[idx_n],main="diseasematerials connections",axes=FALSE,col = c("red","green"))
+    #         axis(2)
+    #         legend(x = "topright",legend = c("Positive","Negative"),fill=c("red","green"))
+    #       }
+    #     })
+ 
+    
+
+  })
+    
+}
